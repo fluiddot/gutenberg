1d9958084fc60cfab1e4cff4b626644a244c7fba
Carlos Garcia <fluiddot@gmail.com>
2021-01-12 16:45:51 +0100

    Enable reusable blocks

--- packages/block-library/src/index.native.js
+++ packages/block-library/src/index.native.js
@@ -224,7 +224,7 @@ export const registerCoreBlocks = () => {
 		socialLinks,
 		pullquote,
 		file,
-		devOnly( reusableBlock ),
+		reusableBlock,
 	].forEach( registerBlock );
 
 	registerBlockVariations( socialLink );
